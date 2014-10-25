9ae91edd3a22235166fbf2827c1daa3d69d9a194
alive-web <alivelp@gmail.com>
2014-10-25 18:56:55 +0300

    It is alive

--- adminUI/my_app/views.py
+++ adminUI/my_app/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from my_app.models import Users
 
 
+
 def index(request):
-    users_list = Users.objects.all().order_by('login')
+    users_list = Users.objects.all().order_by('id')
     context = {'users_list': users_list}
+    if 'delete' in request.POST:
+    	user_id = request.POST['delete']
+    	Users.objects.filter(id=user_id).delete()
+    if  'edit' in request.POST:
+    	id= request.POST['edit']
+    	return redirect('/users/edit/%s'%id)
     return render(request, 'my_app/index.html', context)
+
+def add(request):
+	if "save" in request.POST:
+		user = Users()
+		user.login = request.POST['login']
+		user.fullname = request.POST['fullname']
+		user.token = request.POST['token']
+		user.save()
+		return redirect('index')
+	return render(request, 'my_app/add.html')
+def edit(request, id):
+	if "save" in request.POST:
+		user = Users.objects.get(id=id) 
+		user.login = request.POST['login']
+		user.fullname = request.POST['fullname']
+		user.token = request.POST['token']
+		user.save()
+		return redirect('index')
+	return render(request, 'my_app/add.html')
