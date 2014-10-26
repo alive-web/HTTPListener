f7fff4027a55cd68af9006844330d6a013dacccf
alive-web <alivelp@gmail.com>
2014-10-26 16:07:24 +0200

    add css

--- adminUI/my_app/models.py
+++ adminUI/my_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Users(models.Model):
-    login = models.CharField(max_length=200)
+	login = models.CharField(max_length=200)
     fullname = models.CharField(max_length=200)
     token = models.CharField(max_length=200)
     total_msg_counter = models.IntegerField(default=0)
