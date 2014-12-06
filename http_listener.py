183344ebf6d72c5e3d0f2c0a979a943c0cc15e06
Dovbush <dovbush.it@gmail.com>
2014-12-06 20:50:23 +0200

    Now ND listens to RMQ, send msgs there and receive response

--- http_listener.py
+++ http_listener.py
@@ -8,6 +8,7 @@ import pika
 
 QUEUE_HTTPLISTENER = "httplistener"
 QUEUE_VALIDATION = "validation.messages"
+COUNT = 1
 
 credentials = pika.PlainCredentials('lv128', 'lv128')
 parameters = pika.ConnectionParameters('localhost',
@@ -20,47 +21,47 @@ class HTTPListener(resource.Resource):
     isLeaf = True
 
     def render_GET(self, request):
-         request.setHeader("content-type", "text/plain")
-         message = request.args.get("msg")
-         if message and isinstance(message, list):
-             message = message[0]
-         log.msg(message)
-         return "OK-GET"
+        request.setHeader("content-type", "text/plain")
+        message = request.args.get("msg")
+        if message and isinstance(message, list):
+            message = message[0]
+        log.msg(message)
+        return "OK-GET"
 
     def render_POST(self, request):
-         request.setHeader("content-type", "text/plain")
-         message = request.args.get("msg", "")
-         token = request.args.get("token", "")
-         self.send_msg(QUEUE_VALIDATION, message)
-         
-         if message and isinstance(message, list):
-             message = message[0]
-         if token and isinstance(token, list):
-             token = token[0]
-         triplet = ':'.join([uuid4().hex, token, message])
-
-         with open("validation_queue.log", "a+") as validation_file:
-             validation_file.write(triplet + '\n')
-         
-         #body = self.get_msg(QUEUE_HTTPLISTENER)
-         log.msg(message)
-         return triplet  # for debugging
-
-    def get_msg(self, my_queue):
-        channel = connection.channel()
-        channel.queue_declare(queue=my_queue)
-        for method_frame, properties, body in channel.consume(my_queue):
-            print body
-            log.msg(body)
-            channel.basic_ack(method_frame.delivery_tag)
-            return body
+        request.setHeader("content-type", "text/plain")
+        message = request.args.get("msg", "")
+        token = request.args.get("token", "")
+        if message and isinstance(message, list):
+            message = message[0]
+        if token and isinstance(token, list):
+            token = token[0]
+        triplet = ':'.join([uuid4().hex, token, message])
+        self.send_msg(QUEUE_VALIDATION, triplet)
+        self.get_msg(QUEUE_HTTPLISTENER)
+        with open("validation_queue.log", "a+") as validation_file:
+            validation_file.write(triplet + '\n')
+        log.msg(message)
+        return triplet  # for debugging
         
     def send_msg(self, my_queue, my_msg):
         channel = connection.channel()
-        channel.queue_declare(queue=my_queue)
+        channel.queue_declare(my_queue)
         channel.basic_publish(exchange='', routing_key=my_queue, body=str(my_msg))
         
+    def callback(self, ch, method, properties, body):
+        """this function consume messages and acknowledge them"""
+        
+        log.msg(body)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        return True
 
+    def get_msg(self, my_queue):
+        channel = connection.channel()
+        channel.queue_declare(my_queue)
+        channel.basic_qos(prefetch_count=COUNT)
+        channel.basic_consume(self.callback, queue=my_queue)
+            
 log.startLogging(open('/opt/lv128/log/HTTPListener.log', 'w'))
 endpoints.serverFromString(reactor, "tcp:8812").listen(server.Site(HTTPListener()))
 reactor.run()
